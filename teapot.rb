6c4e7ba76a18d79f1a8790ce1ff4b94e80db3244
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-02-24 13:30:27 +1300

    Initial video4linux libraries

---
+++ teapot.rb
@@ -0,0 +1,22 @@
+
+#
+#  This file is part of the "Teapot" project, and is released under the MIT license.
+#
+
+teapot_version "1.0"
+
+define_target "opencv-linux" do |target|
+	target.provides 'Platform/opencv/linux' do
+		append linkflags [
+			"-ldc1394",
+			"-lswscale",
+			"-lv4l1",
+			"-lv4l2",
+			"-lavcodec",
+			"-lavformat",
+			"-lavutil"
+		]
+	end
+	
+	target.provides :opencv_platform => 'Platform/opencv/linux'
+end
