2988f3f54ff740da9f1cc0c839072f6cbecf701e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-06-11 02:25:32 +1200

    Update teapot version.

--- teapot.rb
+++ teapot.rb
@@ -3,7 +3,7 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "1.0"
+teapot_version "3.0"
 
 define_target "opencv-linux" do |target|
 	target.provides 'Platform/opencv/linux' do
